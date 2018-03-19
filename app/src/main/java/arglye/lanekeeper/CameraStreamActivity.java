499f3e9be40af029d7789a760d93fa7fe0486a9e
j3elam <j3elam@utexas.edu>
2018-03-19 14:14:51 -0500

    add CameraStreamActivity

---
+++ app/src/main/java/arglye/lanekeeper/CameraStreamActivity.java
@@ -0,0 +1,10 @@
+package arglye.lanekeeper;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by johnelam on 3/19/18.
+ */
+
+public class CameraStreamActivity extends AppCompatActivity {
+}
